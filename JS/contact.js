5ed2a345438b966d67159f45c7a894091e91fb50
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-07-22 09:38:51 +0700

    T

---
+++ JS/contact.js
@@ -0,0 +1,34 @@
+
+//show img when user cliked button
+$("#img1").click(function (){
+    Swal.fire({
+        imageUrl: "../img//bodyimg/prize3.jpg",
+        width: "90%",
+        showCancelButton: false,
+        showConfirmButton: false
+      });
+
+});
+
+//show img when user cliked button
+$("#img2").click(function (){
+    Swal.fire({
+        imageUrl: "../img//bodyimg/prize.jpg",
+        width: "90%",
+        showCancelButton: false,
+        showConfirmButton: false
+      });
+
+});
+
+//show img when user cliked button
+$("#img3").click(function (){
+    Swal.fire({
+        imageUrl: "../img//bodyimg/prize1.jpg",
+        width: "90%",
+        showCancelButton: false,
+        showConfirmButton: false
+      });
+
+}); 
+
---
+++ JS/index.js
@@ -0,0 +1,8 @@
+$("#imgbg").click(function () {
+    Swal.fire({
+        imageUrl: "../img//bodyimg/prize1.jpg",
+        width: "90%",
+        showCancelButton: false,
+        showConfirmButton: false
+      }); 
+});
